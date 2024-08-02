5722e6a2d8933ff37fd90b495d0fa3b12a761b59
Paolo Belforte <pbelforte2003@gmail.com>
2024-08-02 16:33:14 -0300

    dead cells with 3 neighbours can resurrect

--- src/conways_game/conways_game.rs
+++ src/conways_game/conways_game.rs
@@ -15,12 +15,14 @@ impl ConwaysGame {
 
     pub fn next_generation(&mut self) {
 
+        let alive_cells = self.alive_cells();
+
+        let resurrected_cells = self.resurrected_cells();
+
         let mut cells = HashSet::new();
 
-        for cell in &self.cells {
-            if self.can_survive(cell) {
-                cells.insert(cell.clone());
-            }
+        for cell in alive_cells.union(&resurrected_cells) {
+            cells.insert(cell.clone());
         }
 
         self.cells = cells;
@@ -28,6 +30,38 @@ impl ConwaysGame {
 
     pub fn is_alive(&self, cell: Point) -> bool {
         self.cells.contains(&cell)
+    }
+
+    fn resurrected_cells(&self) -> HashSet<Point> {
+        let mut cells = HashSet::new();
+
+        for cell in &self.cells {
+            for neigbour in cell.neighbours() {
+                if self.can_resurrect(&neigbour) {
+                    cells.insert(neigbour);
+                }
+            }
+        }
+
+        cells
+    }
+
+    fn alive_cells(&self) -> HashSet<Point> {
+        let mut cells = HashSet::new();
+
+        for cell in &self.cells {
+            if self.can_survive(cell) {
+                cells.insert(cell.clone());
+            }
+
+        }
+        cells
+    }
+
+    fn can_resurrect(&self, cell: &Point) -> bool {
+        let ammount_of_neighbours = cell.neighbours().intersection(&self.cells).count();
+
+        ammount_of_neighbours == 3
     }
 
     fn can_survive(&self, cell: &Point) -> bool {
@@ -75,4 +109,26 @@ mod test {
 
         assert!(conways_game.is_alive(Point::new(0, 0)));
     }
+
+    
+    #[test]
+    fn test_004_a_cell_with_more_than_three_neighbours_dies_after_one_generation() {
+        let cells = vec![Point::new(0, 0), Point::new(0, 1), Point::new(-1, 0), Point::new(1, 0), Point::new(1,1)];
+        let mut conways_game = ConwaysGame::new(cells);
+
+        conways_game.next_generation();
+
+        assert!(!conways_game.is_alive(Point::new(0, 0)));
+    }
+
+    #[test]
+    fn test_005_a_dead_cell_with_exactly_three_neighbours_resurrects_after_one_generation() {
+        let cells = vec![Point::new(0, 1), Point::new(-1, 0), Point::new(1, 0)];
+        let mut conways_game = ConwaysGame::new(cells);
+
+        conways_game.next_generation();
+
+        assert!(conways_game.is_alive(Point::new(0, 0)));
+    }
+
 }
